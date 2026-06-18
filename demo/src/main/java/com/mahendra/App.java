c97e13aa531d84f6460775a7643fd2e57e6cb5cb
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2026-06-18 14:44:06 +0530

    Add Azure and AWS connection strings to App class

--- demo/src/main/java/com/mahendra/App.java
+++ demo/src/main/java/com/mahendra/App.java
@@ -9,7 +9,9 @@ public class App
     String dbConnString = "jdbc:mysql://localhost:3306/demo";
     String dbUser = "root";
     String dbPassword = "root";
-    String azStorageConnString = "DefaultEndpointsProtocol=https;AccountName=mahendra;AccountKey=...;EndpointSuffix=core.windows.net";
+    String azStorageConnString = "DefaultEndpointsProtocol=https;AccountName=mahendra;AccountKey=23t4q724uqyg3ruqg4i5tq75ftgq7t7q26f;EndpointSuffix=core.windows.net";
+    String AWS_ACCESS_KEY = "AKIA5J4Q6Z7333G2F5Q6A";
+    String AWS_SECRET_KEY = "sdfgdfgdfgdfgdfgdf
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
