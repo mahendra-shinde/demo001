afa6e22b88676ef149c9a666b9a00cbdc04bda47
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2026-06-18 14:42:38 +0530

    Add database connection strings to App class

--- demo/src/main/java/com/mahendra/App.java
+++ demo/src/main/java/com/mahendra/App.java
@@ -6,6 +6,10 @@ package com.mahendra;
  */
 public class App 
 {
+    String dbConnString = "jdbc:mysql://localhost:3306/demo";
+    String dbUser = "root";
+    String dbPassword = "root";
+    String azStorageConnString = "DefaultEndpointsProtocol=https;AccountName=mahendra;AccountKey=...;EndpointSuffix=core.windows.net";
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
