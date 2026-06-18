6e6bb4cff9b27bc59b5fce208845fd81bd573ed0
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2026-06-18 14:46:34 +0530

    Refactor AWS and Azure connection strings in App class

--- demo/src/main/java/com/mahendra/App.java
+++ demo/src/main/java/com/mahendra/App.java
@@ -9,9 +9,7 @@ public class App
     String dbConnString = "jdbc:mysql://localhost:3306/demo";
     String dbUser = "root";
     String dbPassword = "root";
-    String azStorageConnString = "DefaultEndpointsProtocol=https;AccountName=mahendra;AccountKey=23t4q724uqyg3ruqg4i5tq75ftgq7t7q26f;EndpointSuffix=core.windows.net";
-    String AWS_ACCESS_KEY = "AKIA5J4Q6Z7333G2F5Q6A";
-    String AWS_SECRET_KEY = "sdfgdfgdfgdfgdfgdf
+    String aws_api_key = "AKIAIOSFODNN7EXAMPLE";
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
